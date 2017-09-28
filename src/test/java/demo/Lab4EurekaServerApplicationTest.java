1d1eb638db026c8354bd3f57a129d0f6679462ed
duke851 <30705332+duke851@users.noreply.github.com>
2017-09-28 23:33:41 +0100

    Add files via upload

---
+++ src/test/java/demo/Lab4EurekaServerApplicationTest.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class Lab4EurekaServerApplicationTest {
+
+	@Test
+	public void testServer() {
+		Assert.assertTrue( 1==1);
+	}
+
+}
